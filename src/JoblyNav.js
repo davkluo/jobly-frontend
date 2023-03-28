df5207d31413cd7295911f09efedce9e255ca342
David Luo <davidkluo@gmail.com>
2023-03-28 12:34:43 -0700

    Make navbar collapsible and change style

--- src/JoblyNav.js
+++ src/JoblyNav.js
@@ -4,6 +4,7 @@ import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { BriefcaseFill } from 'react-bootstrap-icons';
 import './JoblyNav.css';
 
 import userContext from "./userContext";
@@ -19,30 +20,35 @@ function JoblyNav({ logout }) {
   const { user } = useContext(userContext);
 
   return (
-    <Navbar bg="dark" variant="dark">
+    <Navbar bg="dark" variant="dark" expand="md">
       <Container>
-        <Navbar.Brand href="/">Jobly</Navbar.Brand>
-        <Nav className="ms-auto">
-          {!user.isLoggedIn && (
-            <>
-              <Nav.Link href="/login">Login</Nav.Link>
-              <Nav.Link href="/signup">Sign Up</Nav.Link>
-            </>
-          )}
-
-          {user.isLoggedIn && (
-            <>
-              <Nav.Link href="/companies">Companies</Nav.Link>
-              <Nav.Link href="/jobs">Jobs</Nav.Link>
-              <Nav.Link href="/profile">Profile</Nav.Link>
-              <Button
-                className='logoutBtn'
-                onClick={logout}
-                variant="light"
-              >{`Logout: ${user.data.username} (${user.data.firstName})`}</Button>
-            </>
-          )}
-        </Nav>
+        <Navbar.Brand href="/">
+          <BriefcaseFill />
+          Jobly
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="jobly-navbar" />
+        <Navbar.Collapse id="jobly-navbar">
+          <Nav className="ms-auto">
+            {!user.isLoggedIn && (
+              <>
+                <Nav.Link href="/login">Login</Nav.Link>
+                <Nav.Link href="/signup">Sign Up</Nav.Link>
+              </>
+            )}
+            {user.isLoggedIn && (
+              <>
+                <Nav.Link href="/companies">Companies</Nav.Link>
+                <Nav.Link href="/jobs">Jobs</Nav.Link>
+                <Nav.Link href="/profile">Profile</Nav.Link>
+                <Button
+                  className='logoutBtn'
+                  onClick={logout}
+                  variant="light"
+                >{`Logout: ${user.data.firstName}`}</Button>
+              </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
